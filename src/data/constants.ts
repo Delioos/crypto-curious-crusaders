03bcd0acdd78b6fc28ca50e482a0d26549a656c4
delioos <htz_j@protonmail.com>
2023-05-04 00:36:31 +0200

    fix(about): add tricky socials fix(index): removed 3rd title

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -15,12 +15,11 @@ export const MENU_LINKS = [
 
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
-	github1: "https://github.com/chrismwilliams/astro-cactus",
-	twitter1: "https://github.com/chrismwilliams/astro-cactus",
-	discord1: "https://github.com/chrismwilliams/astro-cactus",
-	email1: "rempierr@",
+	tryhackme1: "https://tryhackme.com/p/TricKi",
+	email1: "trickiluckyy@protonmail.com",
+	tg1: "https://t.me/Tricki_LucKy",
 	github2: "https://github.com/delioos",
 	twitter2: "https://twitter.com/deliossssss",
 	email2: "htz_j@protonmail.com",
-	tg2: "https://t.me/delios"
+	tg2: "https://t.me/delios",
 };
